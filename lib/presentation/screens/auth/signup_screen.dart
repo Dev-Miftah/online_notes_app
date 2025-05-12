a425552b42536af548e84ec770ae4ff513164ae4
Miftah <miftahrahi24@gmail.com>
2025-05-13 01:10:28 +0600

    signup screen

--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
-import '../../../routes/app_pages.dart';
 import '../../controllers/auth_controller.dart';
 
 class SignupScreen extends StatelessWidget {
