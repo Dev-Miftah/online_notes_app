17629bae1562f17430fb90e50be94010fcc01716
Miftah <miftahrahi24@gmail.com>
2025-05-13 01:12:09 +0600

    signup email verification screen

--- lib/presentation/screens/auth/waiting_for_verification_screen.dart
+++ lib/presentation/screens/auth/waiting_for_verification_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 import '../../../data/repositories/auth_repository.dart';
-import '../../../routes/app_pages.dart';
 import '../../../utils/custom_toast.dart';
 import '../../controllers/auth_controller.dart';
 
