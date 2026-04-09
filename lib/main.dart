30cacc34d20ecebe3b39206581d26e9c4a286dec
Miftah Uddin Rahi <136071380+Dev-Miftah@users.noreply.github.com>
2026-04-10 00:50:07 +0600

    Remove unnecessary blank line in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:online_notes/app/notes_app.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
